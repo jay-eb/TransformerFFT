2977d29535076d641b2719f8d2bbb4afc8e5e07f
jay-eb <xjie@easternbay.cn>
2025-01-21 11:04:08 +0800

    传统F1计算

--- my_exp/exp/roc.py
+++ my_exp/exp/roc.py
@@ -1,12 +1,16 @@
-from sklearn.metrics import precision_recall_curve, roc_curve, auc
+from sklearn.metrics import precision_recall_curve, roc_curve, auc, classification_report
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 matplotlib.use('TkAgg')
 # 计算 Precision-Recall 曲线
 test_labels = [0]
 test_preds = [1]
 precision, recall, _ = precision_recall_curve(test_labels, test_preds)
+precision1 = precision_score(test_labels, test_preds)
+recall_score1 = precision_score(test_labels, test_preds)
+f1_score1 = f1_score(test_labels, test_preds)
 
 # 可视化
 plt.plot(recall, precision, label="Precision-Recall Curve")
