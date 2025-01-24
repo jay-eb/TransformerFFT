7d4232eace19054679ccec8723646013f328bff5
jay-eb <xjie@easternbay.cn>
2025-01-24 12:00:10 +0800

    doc

---
+++ my_exp/utils/affiliation/testAff.py
@@ -0,0 +1,7 @@
+from my_exp.utils.evaluate import getAffiliationMetrics
+
+
+test_labels = [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1]
+test_preds = [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0]
+a = getAffiliationMetrics(test_labels, test_preds)
+print(a)
